d76ae15958bc0be9445a677108b2c354b9cb82a4
Vlado Cavric <vladocavric@gmail.com>
2022-12-23 23:43:03 +0100

    finished

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -18,7 +18,7 @@ const Modal = ({ ime, name, src, onClose }) => {
 					}`}>
 					<img src={src} alt={name} />
 					<div className='p-15'>
-						<h3 className='title'>{eng ? name : ime}</h3>
+						<h3 className='title mb-10'>{eng ? name : ime}</h3>
 						<div className='d-flex d-flex-end'>
 							<button
 								className='btn btn-primary'
